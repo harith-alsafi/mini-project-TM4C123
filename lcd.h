9646cb96f722ac50b1162768003da82d58f8d240
unknown <Harith Al-Safi>
2023-12-01 16:31:39 +0000

    setup the directory strutcure

---
+++ lcd.h
@@ -0,0 +1,13 @@
+void Clear();
+
+void Init();
+
+void Draw_Pixel(int x, int y);
+
+void Draw_Line(int x1, int y1, int x2, int y2);
+
+void Draw_Horizantal_Line(int x, int y, int length);
+
+void Draw_Vertical_Line(int x, int y, int height);
+
+void Draw_Char(char c);
---
+++ port_init.c
@@ -0,0 +1,19 @@
+#include "port_init.h"
+
+void Port_E_Init(){
+	
+}
+
+
+void Port_D_Init(){
+	
+}
+
+void Port_B_Init(){
+	
+}
+
+
+void Port_A_Init(){
+	
+}
